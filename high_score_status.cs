b58063b4f3f64e82a3508b8249e964e5a6ee4901
AnthonyWalujono <44626057+AnthonyWalujono@users.noreply.github.com>
2019-12-09 23:50:24 -0800

    Create high_score_status.cs

---
+++ high_score_status.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class high_score : MonoBehaviour
+{
+    private Text playerScore;
+    private int highScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = GameObject.Find("Score").GetComponent<Text>();
+        highScore = PlayerPrefs.GetInt("CurrentHighScore");
+
+    }
+    public void updateHighScore()
+    {
+        Debug.Log("UpdatingScore");
+        if (playerScore.GetComponent<score_status>().score > highScore)
+        {
+            highScore += playerScore.GetComponent<score_status>().score;
+            GetComponent<Text>().text = "High Score: " + highScore;
+            PlayerPrefs.SetInt("CurrentHighScore", highScore);
+        }
+
+    }
+    public void OnEnable()
+    {
+        highScore = PlayerPrefs.GetInt("CurrentHighScore");
+        updateHighScore();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (highScore < 0)
+        {
+            highScore = 0;
+        }
+    }
+
+}
